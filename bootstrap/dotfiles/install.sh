fa88999db8fae6234bcb928802290c79891f8eff
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-11 14:43:19 +1000

    fix: Dotfiles install

--- bootstrap/dotfiles/install.sh
+++ bootstrap/dotfiles/install.sh
@@ -3,8 +3,20 @@
 #################
 #### SYMLINKS
 #################
+symlink_if_not_exists() {
+	local symlink_source_path="$1"
+	local symlink_destination_path="$2"
 
-echo "Symlinking astrovim..."
+	if [ ! -d "$symlink_destination_path" ] && [ ! -L "$symlink_destination_path" ]; then
+		echo "Symlinking $symlink_source_path to $symlink_destination_path";
+		ln -s $symlink_source_path $symlink_destination_path;
+	else
+		echo "Symlink or directory already exists at $symlink_destination_path";
+	fi
+}
 
-# Astrovim (nvim)
-ln -s $(pwd)/nvim $HOME/.config/nvim
+symlink_astrovim() {
+	symlink_if_not_exists "$(pwd)/nvim/" "$HOME/.config/nvim"
+}
+
+symlink_astrovim
