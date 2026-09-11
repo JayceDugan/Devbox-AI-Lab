d127b354525d858e64e491c958df0903e643777a
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-11 15:08:42 +1000

    feat: System configuration + RTX power draw limitation

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -7,3 +7,10 @@ echo "Installing dotfiles...";
 
 ./dotfiles/install.sh;
 
+#################
+#### SYSTEM CONFIGURATION
+#################
+echo "Configuring System Services...";
+
+./system-configuration/install.sh;
+
