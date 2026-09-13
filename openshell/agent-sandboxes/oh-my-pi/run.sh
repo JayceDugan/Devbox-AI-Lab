1d2ee6abb4dd2f610ead72f4d8ccf258ba48cf5f
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-13 17:47:09 +1000

    feat: Turn omp agent into harness builder

--- openshell/agent-sandboxes/oh-my-pi/run.sh
+++ openshell/agent-sandboxes/oh-my-pi/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox -- omp --allow-home
+#openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox -- omp --allow-home
+openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox
