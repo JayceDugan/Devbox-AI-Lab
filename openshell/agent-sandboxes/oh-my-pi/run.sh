da077f013d3215b10deacec936e9c20492ba54aa
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-14 19:02:10 +1000

    feat: change software-factory dir to openshell

---
+++ openshell/agent-sandboxes/oh-my-pi/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox -- omp --allow-home
+
+# --no-keep destroys the sandbox after you exit
+openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox --no-keep
--- software-factory/agent-sandboxes/oh-my-pi/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox -- omp --allow-home
-openshell sandbox create --provider my-github --from localhost/ai-lab/openshell-oh-my-pi-sandbox --no-keep
