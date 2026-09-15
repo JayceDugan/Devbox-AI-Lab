70038965151ff7ee3a2085c76f5af74d09b9d444
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-15 19:10:17 +1000

    rage

--- openshell/agent-sandboxes/oh-my-pi/run.sh
+++ openshell/agent-sandboxes/oh-my-pi/run.sh
@@ -3,4 +3,4 @@
 #openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox -- omp --allow-home
 
 # --no-keep destroys the sandbox after you exit
-openshell sandbox create --from localhost/ai-lab/openshell-oh-my-pi-sandbox --no-keep
+openshell sandbox create --provider github --from localhost/ai-lab/openshell-oh-my-pi-sandbox --no-keep
