b88af45c0dd1587bb3d3a29d0d375a403b746fc6
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-26 18:37:15 +1000

    postgres + unsloth:latest + podman container support setup for podman

--- podman/install.sh
+++ podman/install.sh
@@ -3,7 +3,6 @@ set -uex
 
 mkdir -p ~/.config/containers/systemd
 
-
 ln -sf "$PWD/quadlet/unsloth.container" \
     ~/.config/containers/systemd/unsloth.container
 
@@ -12,5 +11,8 @@ ln -sf "$PWD/quadlet/beszel.container" \
 
 ln -sf "$PWD/quadlet/beszel-agent.container" \
     ~/.config/containers/systemd/beszel-agent.container
+
+ln -sf "$PWD/quadlet/postgres.container" \
+    ~/.config/containers/systemd/postgres.container
 
 systemctl --user daemon-reload
