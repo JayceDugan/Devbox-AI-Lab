467ad3c56fa190b83fff42aacd3d9b0d770a2ee9
Jayce Dugan <jgtdpersonal@gmail.com>
2026-09-26 17:06:41 +1000

    feat: Beszel

--- podman/install.sh
+++ podman/install.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+set -uex
+
 mkdir -p ~/.config/containers/systemd
+
 
 ln -sf "$PWD/quadlet/unsloth.container" \
     ~/.config/containers/systemd/unsloth.container
+
+ln -sf "$PWD/quadlet/beszel.container" \
+    ~/.config/containers/systemd/beszel.container
+
+ln -sf "$PWD/quadlet/beszel-agent.container" \
+    ~/.config/containers/systemd/beszel-agent.container
 
 systemctl --user daemon-reload
